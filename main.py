281cf3cad7ccc7f5c48943b6faeab5c6d628fbd9
Tonials <arth.t.s@gmail.com>
2022-06-01 09:50:03 -0300

    update logic

--- main.py
+++ main.py
@@ -1,70 +1,85 @@
+import os
+
+
 def read_words_file(file_name):
     with open(file_name, 'r') as f:
         lines = f.readlines()
-        return [line.strip() for line in lines]
+        return [(line.strip(), 0.0) for line in lines]
 
 
-def initiate_dic():
+def initiate_dictionary():
     aux = {}
     for char in 'abcdefghijklmnopqrstuvwxyz':
         aux[char] = [0, 0, 0, 0, 0, 0]
     return aux
 
 
-def calculate_answers():
-    for word in words_list:
-        for i in range(len(word)):
-            if word[i] not in letters_chance.keys():
-                letters_chance[word[i]] = [0, 0, 0, 0, 0]
-            letters_chance[word[i]][i] += 1
-            letters_chance[word[i]][-1] += 1
+def calculate_letter_incidence():
+    for word_score in words_scores:
+        for i in range(len(word_score[0])):
+            letters_chance[word_score[0][i]][i] += 1
+            letters_chance[word_score[0][i]][-1] += 1
 
 
 def score_words():
-    pass
+    for ws in range(len(words_scores)):
+        aux = list(words_scores[ws])
+        aux[1] = 0.0
+
+        for pos in range(len(aux[0])):
+            aux[1] += 2 * letters_chance[aux[0][pos]][pos]
+            aux[1] += letters_chance[aux[0][pos]][-1]
+
+        words_scores[ws] = tuple(aux)
+
+
+def print_best_words():
+    for i in range(min(10, len(words_scores))):
+        print(f'#{i+1}\t{words_scores[i][0]}\t{words_scores[i][1]}')
 
 
 def letter_not_in_the_word(letter):
-    return [word for word in words_list if letter not in word]
+    return [word_score for word_score in words_scores if letter not in word_score[0]]
 
 
 def letter_in_wrong_place(letter, position):
-    return [word for word in words_list if letter in word]
+    return [word_score for word_score in words_scores if letter in word_score[0] and letter != word_score[0][position]]
 
 
 def letter_in_right_place(letter, position):
-    return [word for word in words_list if letter == word[position]]
+    return [word_score for word_score in words_scores if letter == word_score[0][position]]
 
 
 if __name__ == '__main__':
-    words_list = read_words_file('wordle-allowed-guesses.txt')
-    words_list += read_words_file('wordle-answers-alphabetical.txt')
-    sorted(words_list)
+    words_scores = read_words_file('wordle-allowed-guesses.txt')
+    words_scores += read_words_file('wordle-answers-alphabetical.txt')
+    sorted(words_scores)
 
     attempts = 0
     feedback = ''
 
     while attempts < 6 and feedback != '22222':
-        letters_chance = initiate_dic()
-        calculate_answers()
+        os.system('cls')
 
-        # for i in letters_chance.keys():
-        #     print(i + ': ', end='')
-        #     print(letters_chance[i])
+        letters_chance = initiate_dictionary()
 
-        print(f'\nattempt {attempts + 1}: ', end='')
-        guess = str(input()).lower()
-        print('obtained result: ', end='')
-        feedback = str(input())
+        calculate_letter_incidence()
+        score_words()
+
+        words_scores.sort(key=lambda x: x[1], reverse=True)
+        print_best_words()
+
+        guess = str(input(f'\nattempt {attempts + 1}: ')).lower()
+        feedback = str(input('obtained result: '))
 
         if feedback != '22222':
             for i in range(len(feedback)):
                 if feedback[i] == '0':
-                    words_list = letter_not_in_the_word(guess[i])
+                    words_scores = letter_not_in_the_word(guess[i])
                 if feedback[i] == '1':
-                    words_list = letter_in_wrong_place(guess[i], i)
+                    words_scores = letter_in_wrong_place(guess[i], i)
                 if feedback[i] == '2':
-                    words_list = letter_in_right_place(guess[i], i)
+                    words_scores = letter_in_right_place(guess[i], i)
 
         attempts += 1
 
